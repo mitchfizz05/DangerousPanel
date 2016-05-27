77aa64b6dc4ef6e8fb18e05d3e33c3f974ccec4a
Mitchfizz05 <me@mitchfizz05.net>
2016-05-27 13:52:09 +0930

    Added a Panel Websocket Service which can respond to key requests.

---
+++ PanelServer/PanelWebsocketService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace DangerousPanel_Server.PanelServer
+{
+    public class PanelWebsocketService : WebSocketBehavior
+    {
+        dynamic wscript;
+
+        public PanelWebsocketService()
+        {
+            Type wscriptType = Type.GetTypeFromProgID("WScript.Shell");
+            wscript = System.Activator.CreateInstance(wscriptType);
+        }
+
+        /// <summary>
+        /// Handles "key:" requests
+        /// </summary>
+        /// <param name="data">Message received from client</param>
+        public void OnKeyRequest(string data)
+        {
+            string key = data.Substring("key:".Length, 1);
+            Program.Log("Key request: " + key, debug: true);
+            wscript.SendKeys(key);
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.IsText)
+            {
+                if (e.Data.StartsWith("key:"))
+                {
+                    OnKeyRequest(e.Data);
+                }
+            }
+
+            base.OnMessage(e);
+        }
+    }
+}
