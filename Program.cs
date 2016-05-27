ab3c1b3dddfa86e759eadb8e8c45905d61216e16
Mitchfizz05 <me@mitchfizz05.net>
2016-05-27 13:21:54 +0930

    Set title for console window.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,20 @@ namespace DangerousPanel_Server
 {
     class Program
     {
+        static void Log(string text, ConsoleColor color)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = oldColor;
+        }
+
         static void Main(string[] args)
         {
+            Console.Title = "Dangerous Panel Server";
+            Log("Dangerous Panel Server (by Mitchfizz05)", ConsoleColor.Cyan);
+
+            Console.ReadKey();
         }
     }
 }
